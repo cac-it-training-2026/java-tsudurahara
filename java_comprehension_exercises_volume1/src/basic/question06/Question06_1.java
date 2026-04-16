f51d9af97cfd9e922bff8b81eec6739092ca83de
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-16 13:46:38 +0900

    クエスチョン06

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
@@ -7,6 +7,11 @@ public class Question06_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
-		
+		String[] massages = { "こんにちは", "Java学習中", "繰り返しの演習", "頑張ります" };
+		for (int i = 0; i < 4; i++) {
+			System.out.println(massages[i]);
+		}
+		System.out.println("繰り返しが終了しました。");
+
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,35 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		// TODO:ここに実装
-		
+		int amount = 0;//鉛筆の個数
+		int price = 100;//鉛筆の値段
+
+		System.out.println("鉛筆を購入しますか?");
+		System.out.print("はい：0、いいえ：1 >");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String SbuyFlag = reader.readLine();
+		int buyFlag = Integer.parseInt(SbuyFlag);
+
+		while (buyFlag == 0) {
+			amount++;
+			System.out.println("購入する鉛筆を一本増やしますか？");
+			System.out.print("はい：0、いいえ：1 >");
+			SbuyFlag = reader.readLine();
+			buyFlag = Integer.parseInt(SbuyFlag);
+
+		}
+		System.out.println("購入した鉛筆の本数は" + amount + "本です。");
+		System.out.println("購入した鉛筆の合計金額は" + (amount * price) + "円です。");
+
 	}
 
 }
