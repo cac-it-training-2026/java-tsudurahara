1fcc43c26bb3b33435552f099f21dab08dbf3985
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-16 12:11:55 +0900

    クエスチョン04

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
@@ -11,6 +11,9 @@ public class Question04_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		int[] ageGroup = { 12, 18, 24, 19, 20 };
+
+		System.out.println("3番目の人の年齢は" + ageGroup[2] + "歳です。");
 
 	}
 
