1062a9ae1877d12148a6cc97a07e744da7457790
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-16 12:11:41 +0900

    クエスチョン03

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -9,6 +9,11 @@ public class Question03_1 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		
+		int price = 30;
+		int totalBudget = 300;//購入予算
+		int quantity = totalBudget / price;//購入個数
+
+		System.out.println("商品の購入数は" + quantity + "個です。");
+
 	}
 }
