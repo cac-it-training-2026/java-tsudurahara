bdfc5efa35b13274050a29d2c09a91f6f2405172
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-16 15:14:08 +0900

    クエスチョン06　NO01　修正

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
@@ -7,7 +7,7 @@ public class Question06_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
-		String[] massages = { "こんにちは", "Java学習中", "繰り返しの演習", "頑張ります" };
+		String[] massages = { "こんにちは", "Javaの学習中", "繰り返しの演習", "頑張ります" };
 		for (int i = 0; i < 4; i++) {
 			System.out.println(massages[i]);
 		}
