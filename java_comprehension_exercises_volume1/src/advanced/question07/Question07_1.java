3c668a6d2bf744da0a642a810ef5a0015b4f947d
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-16 14:30:29 +0900

    クエスチョン07 NO01修正

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -1,8 +1,6 @@
 package advanced.question07;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Question07_1 {
@@ -10,8 +8,6 @@ public class Question07_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 		Scanner sc = new Scanner(System.in);
 
 		String[] itemNames = new String[5];
